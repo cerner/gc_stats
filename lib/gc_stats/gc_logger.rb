3421a0c4b0dd8d8b4ef8ad1ba20ad489975170dc
Nathan Beyer <nbeyer@gmail.com>
2015-03-16 21:29:19 -0500

    Rename the the log message field name from 'Process.pid' to 'PID'. The term 'PID' is more succint (conserves characters), but is very well known and recognized, so it doesn't sacrifice any clarity.

--- lib/gc_stats/gc_logger.rb
+++ lib/gc_stats/gc_logger.rb
@@ -11,7 +11,7 @@ module GCStats
       def log_process_action(payload)
         gc_time = GC::Profiler.total_time
         messages = super
-        messages << "Process.pid: #{Process.pid}"
+        messages << "PID: #{Process.pid}"
         messages << "GC: [total_gc_time: #{gc_time*1000} ms | num_events: #{GC::Profiler.raw_data.count} | major/minor/count: #{GC.stat[:major_gc_count]}/#{GC.stat[:minor_gc_count]}/#{GC.count}]" unless gc_time == 0
         messages
       end
--- spec/gc_logger_spec.rb
+++ spec/gc_logger_spec.rb
@@ -16,7 +16,7 @@ describe TestController do
     GC.start
     message = subject.log_process_action(@payload)
     expect(message[0]).to include("test message")
-    expect(message[1]).to match(/Process.pid: \d*/)
+    expect(message[1]).to match(/PID: \d*/)
     expect(message[2]).to match(/GC: \[total_gc_time: \d*.\d* ms \| num_events: \d* \| major\/minor\/count: \d*\/\d*\/\d*\]/)
   end
 
